351a9fd871ca248edc63e186c960409743bc200b
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2025-12-31 14:15:54 +0100

    Update const.py

--- custom_components/ble_adjustable_bed/const.py
+++ custom_components/ble_adjustable_bed/const.py
@@ -1,13 +1,13 @@
 DOMAIN = "ble_adjustable_bed"
 
-DEVICE_NAME = "BK-Light iPixel"
-MANUFACTURER = "BK-Light"
-MODEL = "iPixel 32x32"
+DEVICE_NAME = "Adjustable-Bed Ble"
+MANUFACTURER = "Galaxy"
+MODEL = "26W-N"
 
 # BLE UUIDs (afkomstig uit ESPHome)
-SERVICE_UUID = "000000fa-0000-1000-8000-00805f9b34fb"
-CHARACTERISTIC_UUID = "0000fa02-0000-1000-8000-00805f9b34fb"
+SERVICE_UUID = "0000fee9-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_UUID = "d44bc439-abfd-45a2-b575-925416129600"
 
-# Power commands
-POWER_ON_COMMAND = bytearray([0x05, 0x00, 0x07, 0x01, 0x01])
-POWER_OFF_COMMAND = bytearray([0x05, 0x00, 0x07, 0x01, 0x00])
+# Power commands zg and flad
+POWER_ON_COMMAND = bytearray([0x6e, 0x01, 0x00, 0x45, 0xb4])
+POWER_OFF_COMMAND = bytearray([0x6e, 0x01, 0x00, 0x31, 0xa0])
