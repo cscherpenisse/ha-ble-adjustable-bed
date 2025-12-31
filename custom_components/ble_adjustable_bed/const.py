ae2ca0e0c7b8e2a9161f20246614e9cf469a8ee3
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2025-12-31 19:32:35 +0100

    Update const.py

--- custom_components/ble_adjustable_bed/const.py
+++ custom_components/ble_adjustable_bed/const.py
@@ -8,6 +8,15 @@ MODEL = "BLE Adjustable Bed"
 BED_SERVICE_UUID = "0000fee9-0000-1000-8000-00805f9b34fb"
 BED_CHAR_UUID = "d44bc439-abfd-45a2-b575-925416129600"
 
+# Simulated movement
+COVER_MOVE_STEP = 5          # % per actie
+COVER_MOVE_DELAY = 0.3      # seconden
+
+HEAD_UP_CMD = "head_up"
+HEAD_DOWN_CMD = "head_down"
+FEET_UP_CMD = "feet_up"
+FEET_DOWN_CMD = "feet_down"
+
 # Bed commands (5 bytes each)
 BED_COMMANDS = {
     "light": bytearray([0x6E, 0x01, 0x00, 0x3C, 0xAB]),
