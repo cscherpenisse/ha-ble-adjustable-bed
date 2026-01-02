b1bc22c78e4e4dab738e5def99ed186d24da1bc2
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-01-02 09:56:49 +0100

    Update number.py

--- custom_components/ble_adjustable_bed/number.py
+++ custom_components/ble_adjustable_bed/number.py
@@ -9,20 +9,17 @@ from .const import (
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities(
-        [
-            BedStepsNumber(
-                entry,
-                name="Head Steps",
-                object_id="bed_head_steps",
-            ),
-            BedStepsNumber(
-                entry,
-                name="Feet Steps",
-                object_id="bed_feet_steps",
-            ),
-        ]
-    )
+    data = hass.data[DOMAIN][entry.entry_id]
+    data.setdefault("numbers", {})
+
+    head = BedStepsNumber(entry, "Head Steps", "head")
+    feet = BedStepsNumber(entry, "Feet Steps", "feet")
+
+    data["numbers"]["head"] = head
+    data["numbers"]["feet"] = feet
+
+    async_add_entities([head, feet])
+
 
 
 class BedStepsNumber(NumberEntity):
@@ -33,11 +30,13 @@ class BedStepsNumber(NumberEntity):
     _attr_step = 1
     _attr_mode = "box"
 
-    def __init__(self, entry, name, object_id):
+    def __init__(self, entry, name, key):
         self.entry = entry
+        self.key = key
         self._attr_name = f"{DEVICE_NAME} {name}"
-        self._attr_unique_id = f"{entry.entry_id}_{object_id}"
-        self._attr_native_value = 10  # default
+        self._attr_unique_id = f"{entry.entry_id}_{key}_steps"
+        self._attr_native_value = 10
+
 
     @property
     def device_info(self):
